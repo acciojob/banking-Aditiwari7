c00da03b75322205a47ebbd3ac878225d0c07c65
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2022-12-30 15:56:32 +0530

    initial

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -6,6 +6,7 @@ public class BankAccount {
     private double balance;
     private double minBalance;
 
+    public BankAccount(){}
     public BankAccount(String name, double balance, double minBalance) {
 
     }

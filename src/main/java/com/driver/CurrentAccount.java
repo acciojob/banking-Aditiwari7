4d776d86497b1e4f47a6cc827b0a0217d180c953
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2022-12-30 18:03:06 +0530

    try

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,7 +5,8 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        super(name, balance, 5000);
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,7 +14,57 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+        String curr = this.tradeLicenseId;
+        int len = curr.length();
+        boolean isValid = true;
+        for(int i=1; i<len; i++){
+            if(curr.charAt(i) == curr.charAt(i-1))
+                isValid = false;
+        }
 
+        String ans = "";
+
+        if(isValid == false){
+            int[] charMap = new int[26];
+            char mostUsedChar = curr.charAt(0);
+            for(char c : curr.toCharArray()){
+                charMap[c-'a']++;
+                if(charMap[c-'a'] > charMap[mostUsedChar-'a']){
+                    mostUsedChar = c;
+                }
+            }
+
+            char[] ansArray = new char[curr.length()];
+            int ansArrayIndex = 0;
+            for(int i=-1; i<charMap.length; i++){
+                char currCh;
+                if(i==-1){
+                    currCh = mostUsedChar;
+                }else{
+                    currCh = (char)(i+'a');
+                }
+
+                while(charMap[currCh-'a'] > 0){
+                    if((ansArrayIndex>0 && ansArray[ansArrayIndex-1]==currCh) || (ansArrayIndex<ansArray.length-1 && ansArray[ansArrayIndex+1]==currCh)){
+                        isValid = false;
+                        throw new Exception("Valid License can not be generated");
+                    }
+                    ansArray[ansArrayIndex] = currCh;
+
+                    if(ansArrayIndex+2 < ansArray.length){
+                        ansArrayIndex += 2;
+                    }else{
+                        ansArrayIndex = 1;
+                    }
+                    charMap[currCh-'a']--;
+                }
+            }
+            ans = new String(ansArray);
+        }
+
+        this.tradeLicenseId = ans;
     }
 
+    public String getTradeLicenseId() {return tradeLicenseId;}
+    public void setTradeLicenseId(String tradeLicenseId) {this.tradeLicenseId = tradeLicenseId;}
 }
